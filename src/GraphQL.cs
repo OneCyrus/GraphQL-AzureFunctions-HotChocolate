8df92ed6698a9833b6feb4cfc0103f5aa4fb93ca
Daniel Gut <daniel.gut@gia.ch>
2020-01-09 10:59:35 +0100

    feat(parser): added HC parser

--- src/GraphQL.cs
+++ src/GraphQL.cs
@@ -1,37 +1,76 @@
 using HotChocolate.Execution;
+using HotChocolate.Language;
+using HotChocolate.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GraphQLAzureFunctions
 {
     public class GraphQL
     {
+        private readonly IDocumentCache _documentCache;
+        private readonly IDocumentHashProvider _documentHashProvider;
         private readonly IQueryExecutor _executor;
 
-        public GraphQL(IQueryExecutor executor)
+        public GraphQL(IQueryExecutor executor, IDocumentCache documentCache,
+            IDocumentHashProvider documentHashProvider)
         {
             _executor = executor;
+            _documentCache = documentCache;
+            _documentHashProvider = documentHashProvider;
         }
 
         [FunctionName("graphql")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, CancellationToken cancellationToken)
         {
-            var graphQLRequest = JsonConvert.DeserializeObject<GraphQLRequest>(await req.ReadAsStringAsync());
+            var maxRequestSize = 20 * 1000 * 1000;
 
-            log.LogInformation(graphQLRequest.Query);
+            var _requestHelper = new RequestHelper(
+               _documentCache,
+               _documentHashProvider,
+               maxRequestSize,
+               new ParserOptions());
 
-            var query = QueryRequestBuilder.New()
-                   .SetQuery(graphQLRequest.Query)
-                   .SetOperation(graphQLRequest.OperationName);
+            var builder = QueryRequestBuilder.New();
 
-            var result = await _executor.ExecuteAsync(query.Create());
+            using (Stream stream = req.HttpContext.Request.Body)
+            {
+                var requestQuery = await _requestHelper
+                    .ReadJsonRequestAsync(stream, cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (requestQuery.Count > 0)
+                {
+                    var firstQuery = requestQuery[0];
+
+                    builder
+                        .SetQuery(firstQuery.Query)
+                        .SetOperation(firstQuery.OperationName)
+                        .SetQueryName(firstQuery.QueryName);
+
+                    if (firstQuery.Variables != null
+                        && firstQuery.Variables.Count > 0)
+                    {
+                        builder.SetVariableValues(firstQuery.Variables);
+                    }
+                }
+                else
+                {
+                    return new NotFoundObjectResult(null);
+                }
+            }
+
+            var result = await _executor.ExecuteAsync(builder.Create());
 
             return new OkObjectResult(result);
         }
